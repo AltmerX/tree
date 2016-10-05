4a943caabedf847df7e228fe7fb3e3c97bdb0427
AltmerX <sabrina5426@live.com>
2016-10-05 00:54:05 -0400

    Update pytree.py

--- pytree.py
+++ pytree.py
@@ -4,46 +4,46 @@ import sys
 import os
 
 
-
 def display(path):
     global totalDirCount
     global totalFileCount
     print(path)
-    tree(path,1,"")
-    print("\n",totalDirCount," directories, ",totalFileCount," files",sep='')
+    tree(path, 1, "")
+    print("\n", totalDirCount, " directories, ", totalFileCount, " files", sep='')
 
-def tree(path,level,indent):
+    
+def tree(path, level, indent):
     global totalDirCount
     global totalFileCount
-    content=sorted(os.listdir(path))
-    count=0
+    content = sorted(os.listdir(path))
+    count = 0
     for entry in content:
-        if os.path.isfile(path+"/"+entry):
-            totalFileCount+=1
-            if count==len(content)-1:
-                temp=indent+"└── "
-                print(temp+entry)
+        if os.path.isfile(path + "/" + entry):
+            totalFileCount += 1
+            if count == len(content) - 1:
+                temp = indent + "└── "
+                print(temp + entry)
             else:
-                temp=indent+"├── "
-                print(temp+entry)
+                temp = indent + "├── "
+                print(temp + entry)
         else:
-            totalDirCount+=1
-            if count==len(content)-1:
-                temp=indent+"└── "
-                print(temp+entry)
-                indent=indent+"    "
-                tree(path+"/"+entry,level+1,indent)
+            totalDirCount += 1
+            if count == len(content) - 1:
+                temp = indent + "└── "
+                print(temp + entry)
+                indent = indent + "    "
+                tree(path + "/" + entry, level + 1, indent)
             else:
-                print(indent+"├── "+entry)
-                tree(path+"/"+entry,level+1,indent+"│   ")
-        count=count+1
-        
+                print(indent + "├── " + entry)
+                tree(path + "/" + entry, level + 1, indent + "│   ")
+        count = count + 1
+
 if __name__ == '__main__':
     # just for demo
-    path="."
-    totalDirCount=0
-    totalFileCount=0
-    if len(sys.argv)==2:
-        path=sys.argv[1]
+    path = "."
+    totalDirCount = 0
+    totalFileCount = 0
+    if len(sys.argv) == 2:
+        path = sys.argv[1]
     display(path)
     
